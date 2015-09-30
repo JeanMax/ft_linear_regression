eba2f5501097430444c8df11964cb2dc0ecec8d9
Max CANAL <mcanal@e2r7p6.42.fr>
2015-09-30 18:18:10 +0200

    float oversmurfing fix :)

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ special thanks to: rbobillo, bjacob <3
 
 ### TODO: ###
 
-* mac os tests
-* GUI
-* calculate precision of algo?
+* yay
--- src/Train.scala
+++ src/Train.scala
@@ -22,13 +22,13 @@ import math.abs //do the math
 
 object Train
 {
-	val scale: Float = 0.001f
-	val lRate: Float = 0.15f * scale
-	val limit: Float = 0.0000005f
+	val scale: Double = 0.001
+	val lRate: Double = 0.15 * scale
+	val limit: Double = 0.000000001
 
 	//calculate the precision on t0 and t1
-	def errorRate(data: List[Array[Float]], t0: Float, t1: Float, sum: Float): 
-			Float =
+	def errorRate(data: List[Array[Double]], t0: Double, t1: Double, sum: Double): 
+			Double =
 	{
 		if (data.tail == Nil)
 			return ((sum + abs(data.head(1) - (t0 + t1 * data.head(0)))
@@ -39,8 +39,8 @@ object Train
 	}
 
 	//calculate the sum needed in t0 formulae
-	def sumT0(data: List[Array[Float]], t0: Float, t1: Float, sum: Float): 
-			Float =
+	def sumT0(data: List[Array[Double]], t0: Double, t1: Double, sum: Double): 
+			Double =
 	{
 		if (data.tail == Nil)
 			return (sum + t1 * data.head(0) - data.head(1) + t0)
@@ -49,8 +49,8 @@ object Train
 	}
 
 	//calculate the sum needed in t1 formulae
-	def sumT1(data: List[Array[Float]], t0: Float, t1: Float, sum: Float): 
-			Float =
+	def sumT1(data: List[Array[Double]], t0: Double, t1: Double, sum: Double): 
+			Double =
 	{
 		if (data.tail == Nil)
 			return (sum + (t1 * data.head(0)- data.head(1) + t0) * data.head(0))
@@ -60,11 +60,11 @@ object Train
 	}
 
 	//calculate t0 and t1
-	def doTheMath(data: List[Array[Float]], t0: Float, t1: Float):
-			(Float, Float) =
+	def doTheMath(data: List[Array[Double]], t0: Double, t1: Double):
+			(Double, Double) =
 	{
-		val tmpT0: Float = lRate / data.length * sumT0(data, t0, t1, 0)
-		val tmpT1: Float = lRate / data.length * sumT1(data, t0, t1, 0)
+		val tmpT0: Double = lRate / data.length * sumT0(data, t0, t1, 0)
+		val tmpT1: Double = lRate / data.length * sumT1(data, t0, t1, 0)
 
 		if (abs(tmpT0) < limit && abs(tmpT1) < limit)
 			return (t0 / scale, t1)
@@ -73,16 +73,16 @@ object Train
 	}
 
 	//parse data.csv into a pretty list
-	def readFile: List[Array[Float]] =
+	def readFile: List[Array[Double]] =
 	{
-		var ret: List[Array[Float]] = Nil
+		var ret: List[Array[Double]] = Nil
 
 		try
 		{
 			for (s <- fromFile("data/data.csv").getLines.toList
 					.map(_.split(",")).filter(_(0) != "km"))
 				ret = ret ::: List(Array(
-					s(0).toFloat * scale, s(1).toFloat * scale))
+					s(0).toDouble * scale, s(1).toDouble * scale))
 		}
 		catch
 		{
